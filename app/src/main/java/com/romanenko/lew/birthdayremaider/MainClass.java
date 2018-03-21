380faccc92d102b37923bc701fe61f9369cf6320
Lev <romanenko.lew@gmail.com>
2018-03-21 12:30:57 +0200

    There was created Main class and main activity

---
+++ app/src/main/java/com/romanenko/lew/birthdayremaider/MainClass.java
@@ -0,0 +1,21 @@
+package com.romanenko.lew.birthdayremaider;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.romanenko.lew.birthdayremaider.View.MainActivity;
+
+/**
+ * Created by Lev- on 20.03.2018.
+ */
+
+public class MainClass extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
